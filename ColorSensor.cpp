61d511c39090e2622330962aba3fcdb219811200
Alberto <albertojahueymoncada@gmail.com>
2017-11-13 23:43:20 -0600

    Deleted unused Serial calls

--- ColorSensor.cpp
+++ ColorSensor.cpp
@@ -9,22 +9,12 @@ ColorSensor::ColorSensor(ShiftRegister74HC595& sr) : sr(sr){
 Color ColorSensor::getColor(){ 
   sr.set(S2,LOW);
   sr.set(S3,LOW);
-  // Reading the output frequency
   redFrequency = pulseIn(sensorOut, LOW);
-  // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(redFrequency);//printing RED color frequency
-  Serial.print("  ");
+
  
-  // Setting Blue filtered photodiodes to be read
   sr.set(S2,LOW);
   sr.set(S3,HIGH);
-  // Reading the output frequency
   blueFrequency = pulseIn(sensorOut, LOW);
-  // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(blueFrequency);//printing RED color frequency
-  Serial.println("  ");
 
   if(redFrequency > 400 || blueFrequency > 400){
     return Color::None;
